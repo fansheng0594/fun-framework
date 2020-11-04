5d94fc65619cf03f3cbacf3b82ae58498a4da302
Append <appendtrue@gmail.com>
2020-11-04 16:16:12 +0800

    Contracts/container

--- index.php
+++ index.php
@@ -3,5 +3,5 @@
 require __DIR__.'/vendor/autoload.php';
 
 $app = new Fantastic\Foundation\Application(
-    $_ENV['APP_BASE_PATH'] ?? dirname(__DIR__)
+    $_ENV['APP_BASE_PATH'] ?? __DIR__
 );
---
+++ src/Fantastic/Foundation/Application.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Fantastic\Foundation;
+
+class Application
+{
+    public function __construct($arg)
+    {
+        var_dump($arg);
+    }
+}
